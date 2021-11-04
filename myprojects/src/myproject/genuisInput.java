5ae10799df0a81953edb0a6804f40b13f9966ed5
HGP49 <hgp49@Thes-Macintosh-2.local>
2021-11-03 19:38:29 -0700

    create output program

---
+++ myprojects/src/myproject/genuisInput.java
@@ -0,0 +1,30 @@
+package myproject;
+
+import java.util.Scanner;
+
+public class genuisInput {
+	
+   
+	public static void main(String[] args) { ///method
+          System.out.println("What is your name?");
+           ///type identifier = new  type();
+          Scanner scanner = new Scanner(System.in);
+           String name = scanner.nextLine();
+           System.out.println("hello" +  name);
+           
+           
+           /* Teammates Code Goes Here*/
+           
+           
+           
+           
+	}
+}
+	
+	
+	//( ) - Paranthesis
+	// { } - Curly Brackets
+	// [ ] - Square Brackets
+	//" " - Double Quotes
+	
+         
